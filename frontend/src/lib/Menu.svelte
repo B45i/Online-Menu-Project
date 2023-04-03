d0be7e597c38f2c6cd992b591c5c7ddacb5fd190
B45i <amalshajan2011@gmail.com>
2023-04-03 23:03:48 +0530

    bootstrap

--- frontend/src/lib/Menu.svelte
+++ frontend/src/lib/Menu.svelte
@@ -1,17 +1,106 @@
 <script>
     import { onMount } from 'svelte';
-    import { getMenu, placeOrder } from '../api/api';
 
-    let menuItems = [];
+    let menuItems = [
+        {
+            id: 1,
+            name: 'Butter Chicken',
+            price: 250,
+            image_url: 'https://www.example.com/butter_chicken.jpg',
+        },
+        {
+            id: 2,
+            name: 'Chicken Tikka Masala',
+            price: 200,
+            image_url: 'https://www.example.com/chicken_tikka_masala.jpg',
+        },
+        {
+            id: 3,
+            name: 'Aloo Gobi',
+            price: 150,
+            image_url: 'https://www.example.com/aloo_gobi.jpg',
+        },
+        {
+            id: 4,
+            name: 'Palak Paneer',
+            price: 180,
+            image_url: 'https://www.example.com/palak_paneer.jpg',
+        },
+        {
+            id: 5,
+            name: 'Samosa',
+            price: 120,
+            image_url: 'https://www.example.com/samosa.jpg',
+        },
+        {
+            id: 6,
+            name: 'Chana Masala',
+            price: 160,
+            image_url: 'https://www.example.com/chana_masala.jpg',
+        },
+        {
+            id: 7,
+            name: 'Dal Makhani',
+            price: 220,
+            image_url: 'https://www.example.com/dal_makhani.jpg',
+        },
+        {
+            id: 8,
+            name: 'Paneer Tikka',
+            price: 280,
+            image_url: 'https://www.example.com/paneer_tikka.jpg',
+        },
+        {
+            id: 9,
+            name: 'Biryani',
+            price: 350,
+            image_url: 'https://www.example.com/biryani.jpg',
+        },
+        {
+            id: 10,
+            name: 'Naan',
+            price: 100,
+            image_url: 'https://www.example.com/naan.jpg',
+        },
+        {
+            id: 11,
+            name: 'Lassi',
+            price: 120,
+            image_url: 'https://www.example.com/lassi.jpg',
+        },
+        {
+            id: 12,
+            name: 'Raita',
+            price: 100,
+            image_url: 'https://www.example.com/raita.jpg',
+        },
+        {
+            id: 13,
+            name: 'Papadum',
+            price: 80,
+            image_url: 'https://www.example.com/papadum.jpg',
+        },
+        {
+            id: 14,
+            name: 'Rogan Josh',
+            price: 300,
+            image_url: 'https://www.example.com/rogan_josh.jpg',
+        },
+        {
+            id: 15,
+            name: 'Gulab Jamun',
+            price: 120,
+            image_url: 'https://www.example.com/gulab_jamun.jpg',
+        },
+    ];
     let showCartItems = false;
     let cart = [];
-    $: total =
-        cart.reduce((acc, cur) => acc + cur.quantity * cur.price, 0) || 0;
+    let total = 0;
 
     onMount(loadMenu);
 
     async function loadMenu() {
-        menuItems = await getMenu();
+        // menuItems = await getMenu();
     }
 
     function toggleShowCart() {
@@ -19,18 +108,25 @@
     }
 
     function addItemToCart(item) {
-        const cartItem = cart.find(x => x.id === item.id);
-        if (cartItem) {
-            cartItem.quantity++;
-            cart = [...cart];
-        } else {
-            cart = [...cart, { ...item, quantity: 1 }];
-        }
+        // const cartItem = cart.find(x => x.id === item.id);
+        cart = [...cart, item]; // array destructuring
+
+        // if (cartItem) {
+        //     cartItem.quantity++;
+        //     cart = [...cart];
+        // } else {
+        //     cart = [...cart, { ...item, quantity: 1 }];
+        // }
+
+        total = 0;
+        cart.forEach(item => {
+            total += item.price;
+        });
     }
 
     async function handlePlaceOrder() {
         try {
-            const response = await placeOrder({ items: cart, seat_id: 'A1' });
+            // const response = await placeOrder({ items: cart, seat_id: 'A1' });
             cart = [];
         } catch (error) {
             console.log(error);
@@ -39,46 +135,46 @@
 </script>
 
 <main>
-    <nav>Menu</nav>
+    <!-- navbar -->
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Hotel ABC</a>
+        </div>
+    </nav>
 
     <!-- menu -->
+
     <div class="menu">
-        <ul class="menu-list">
-            {#each menuItems as item}
-                <li class="menu-item">
-                    <img class="item-image" src={item.image_url} alt="food" />
-                    <div class="item-info">
-                        <h4>{item.name}</h4>
-                        Rs.<span>{item.price}</span>
-                    </div>
-                    <button on:click={e => addItemToCart(item)}>Add</button>
-                </li>
-            {/each}
-        </ul>
+        {#each menuItems as item}
+            <div class="menu-item">
+                <img
+                    class="menu-image"
+                    src="https://images.unsplash.com/photo-1630409346824-4f0e7b080087?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2146&q=80"
+                    alt=""
+                    srcset=""
+                />
+
+                <div class="flex-grow-1">
+                    <div class="fw-bold">{item.name}</div>
+                    <div class="badge text-bg-success">Rs. {item.price}</div>
+                </div>
+
+                <button
+                    class="btn btn-success"
+                    on:click={e => addItemToCart(item)}
+                >
+                    Add to cart
+                </button>
+            </div>
+        {/each}
     </div>
 
     <!-- cart -->
     <div class="cart">
-        {#if showCartItems}
-            <ul>
-                {#each cart as item}
-                    <li class="cart-item">
-                        <span>{item.name} X {item.quantity}</span>
-                        <button>Remove</button>
-                    </li>
-                {/each}
-            </ul>
-        {/if}
-
-        <div class="cart-menu">
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <span class="show-cart" on:click={toggleShowCart}>
-                {showCartItems ? 'Hide Cart' : 'Show Cart'}
-            </span>
-            <div>
-                Total: <span>{total} </span> Rs
-                <button on:click={handlePlaceOrder}>Order</button>
-            </div>
+        <button class="btn btn-primary">Show Cart</button>
+        <div class="d-flex align-items-center gap-3">
+            <span class="badge text-bg-success">Total: {total}</span>
+            <button class="btn btn-primary">Place order</button>
         </div>
     </div>
 </main>
@@ -86,64 +182,31 @@
 <style>
     .menu {
         flex-grow: 1;
+        padding: 1rem;
         overflow: auto;
     }
 
-    .menu-list {
-        margin: 1rem;
-    }
-
     .menu-item {
-        background: #fff;
-        margin-bottom: 10px;
-        padding: 1rem;
-        border-radius: 5px;
         display: flex;
-        gap: 1rem;
         align-items: center;
-        box-shadow: rgba(0, 0, 0, 0.15) 0px 4px 12px;
-        transition: all 0.3s;
-    }
-
-    .menu-item:hover {
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 4px 12px;
-    }
-
-    .item-image {
-        height: 4rem;
-        width: 6rem;
-        border-radius: 5px;
-        object-fit: cover;
+        gap: 1rem;
+        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        padding: 1rem;
+        margin-bottom: 1rem;
         border-radius: 5px;
     }
 
-    .item-info {
-        flex-grow: 1;
+    .menu-image {
+        max-width: 4rem;
+        border-radius: 5px;
     }
 
     .cart {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
         padding: 1rem;
-        background-color: black;
-        color: white;
-        border-radius: 8px 8px 0 0;
-    }
-
-    .cart-menu {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .show-cart {
-        text-decoration: underline;
-        cursor: pointer;
-        margin-right: auto;
-    }
-
-    .cart-item {
-        margin-bottom: 1rem;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+        background-color: #000;
+        color: #fff;
     }
 </style>
