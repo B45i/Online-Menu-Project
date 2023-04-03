62ba60aef70de480a2d7703e0a52cd6647489eee
amal.shajan <amalshajan2011@gmail.com>
2023-04-03 21:26:58 +0530

    basic navigation

--- frontend/src/lib/AdminNav.svelte
+++ frontend/src/lib/AdminNav.svelte
@@ -1,1 +1,16 @@
-<nav>Admin</nav>
+<script>
+    import { Link } from 'svelte-navigator';
+</script>
+
+<nav>
+    <h4>Admin</h4>
+    <Link to="/admin/orders">Orders</Link>
+    <Link to="">Menu</Link>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        gap: 1rem;
+    }
+</style>
