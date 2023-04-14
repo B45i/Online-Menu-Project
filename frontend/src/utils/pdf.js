9374bdcfeb8f2373f7702826dd2ebb52ca4ff8e2
B45i <amalshajan2011@gmail.com>
2023-04-14 22:20:25 +0530

    removing pdf

--- frontend/src/utils/pdf.js
+++ frontend/src/utils/pdf.js
@@ -1,54 +1,52 @@
 import dayjs from 'dayjs';
 import * as pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-// @ts-ignore
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+// import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+// // @ts-ignore
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export function generatePDF(restaurantName, itemList, table, id) {
-    let totalAmount = 12;
-    const docDefinition = {
-        pageSize: { width: 500, height: 600 },
-        content: [
-            { text: restaurantName, style: 'header' },
-            {
-                text: dayjs().format('D-MM-YYYY  hh:mm A'),
-            },
-            { text: '\n' },
-
-            { text: `Order ID: ${id}, Table: ${table}` },
-            { text: '\n' },
-            { text: 'Items:' },
-            {
-                table: {
-                    headerRows: 1,
-                    widths: ['*', '*', '*'],
-                    body: [
-                        ['Item', 'Quantity', 'Price'],
-                        ...itemList.map(item => [
-                            item.name,
-                            item.quantity,
-                            item.price,
-                        ]),
-                    ],
-                },
-            },
-            { text: `Total Amount:  ${totalAmount} Rs.`, style: 'subheader' },
-        ],
-        styles: {
-            header: {
-                fontSize: 22,
-                bold: true,
-                alignment: 'center',
-                margin: [0, 0, 0, 20],
-            },
-            subheader: {
-                fontSize: 18,
-                bold: true,
-                margin: [0, 15, 0, 5],
-            },
-        },
-    };
-
+    // let totalAmount = 12;
+    // const docDefinition = {
+    //     pageSize: { width: 500, height: 600 },
+    //     content: [
+    //         { text: restaurantName, style: 'header' },
+    //         {
+    //             text: dayjs().format('D-MM-YYYY  hh:mm A'),
+    //         },
+    //         { text: '\n' },
+    //         { text: `Order ID: ${id}, Table: ${table}` },
+    //         { text: '\n' },
+    //         { text: 'Items:' },
+    //         {
+    //             table: {
+    //                 headerRows: 1,
+    //                 widths: ['*', '*', '*'],
+    //                 body: [
+    //                     ['Item', 'Quantity', 'Price'],
+    //                     ...itemList.map(item => [
+    //                         item.name,
+    //                         item.quantity,
+    //                         item.price,
+    //                     ]),
+    //                 ],
+    //             },
+    //         },
+    //         { text: `Total Amount:  ${totalAmount} Rs.`, style: 'subheader' },
+    //     ],
+    //     styles: {
+    //         header: {
+    //             fontSize: 22,
+    //             bold: true,
+    //             alignment: 'center',
+    //             margin: [0, 0, 0, 20],
+    //         },
+    //         subheader: {
+    //             fontSize: 18,
+    //             bold: true,
+    //             margin: [0, 15, 0, 5],
+    //         },
+    //     },
+    // };
     // @ts-ignore
-    pdfMake.createPdf(docDefinition).open();
+    // pdfMake.createPdf(docDefinition).open();
 }
